a77fcd3b72013aee0c24ebaf0cae2214efc31ccf
unknown <felipeplayf13@gmail.com>
2026-09-21 11:05:02 -0300

    testando group by

---
+++ 12_groupby.sql
@@ -0,0 +1,6 @@
+SELECT IdCliente, sum(QtdePontos), COUNT(IdTransacao)
+FROM transacoes
+WHERE DtCriacao >= '2025-07-01' AND DtCriacao > '2025-08-01'
+GROUP BY IdCliente
+ORDER BY SUM(QtdePontos) DESC
+LIMIT 10
