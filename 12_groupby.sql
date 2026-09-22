19bf4b2e61c75e13b1792e6d2a1b3281ae5a265f
unknown <felipeplayf13@gmail.com>
2026-09-22 13:29:51 -0300

    primeira query com HAVING

--- 12_groupby.sql
+++ 12_groupby.sql
@@ -2,5 +2,7 @@ SELECT IdCliente, sum(QtdePontos), COUNT(IdTransacao)
 FROM transacoes
 WHERE DtCriacao >= '2025-07-01' AND DtCriacao > '2025-08-01'
 GROUP BY IdCliente
+--Having vai sergir para filtrar dados depois do agrupamento
+HAVING sum(qtdePontos) >= 4000
 ORDER BY SUM(QtdePontos) DESC
 LIMIT 10
